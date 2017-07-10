f1f5e2ebf967aec31fd463df2019a48cc48c8f4e
Pedro Freitas <pedrosffreitas@gmail.com>
2017-07-09 23:30:38 -0300

    nothing really, just messing around.

--- esp8266/blinking/blink.c
+++ esp8266/blinking/blink.c
@@ -8,31 +8,31 @@
 static const int pin = 2;
 static volatile os_timer_t some_timer;
 
-void some_timerfunc(void *arg)
+void some_timerfunc(void)
 {
-  //Do blinky stuff
-  if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << pin))
-  {
-    // set gpio low
-    gpio_output_set(0, (1 << pin), 0, 0);
-  }
-  else
-  {
-    // set gpio high
-    gpio_output_set((1 << pin), 0, 0, 0);
-  }
+    //Do blinky stuff
+    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << pin))
+    {
+        // set gpio low
+        gpio_output_set(0, (1 << pin), 0, 0);
+    }
+    else
+    {
+        // set gpio high
+        gpio_output_set((1 << pin), 0, 0, 0);
+    }
 }
 
-void ICACHE_FLASH_ATTR user_init()
+void ICACHE_FLASH_ATTR user_init(void)
 {
-  // init gpio subsytem
-  gpio_init();
+    // init gpio subsytem
+    gpio_init();
 
-  // configure UART TXD to be GPIO1, set as output
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1); 
-  gpio_output_set(0, 0, (1 << pin), 0);
+    // configure UART TXD to be GPIO1, set as output
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1);
+    gpio_output_set(0, 0, (1 << pin), 0);
 
-  // setup timer (500ms, repeating)
-  os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, NULL);
-  os_timer_arm(&some_timer, 900, 1);
+    // setup timer (500ms, repeating)
+    os_timer_setfn(&some_timer, some_timerfunc, NULL);
+    os_timer_arm(&some_timer, 500, 1);
 }
