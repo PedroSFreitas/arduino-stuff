9fb5c2af9b4927bdf47e4e114ba7bc38317ffead
Pedro Freitas <pedrosffreitas@gmail.com>
2017-07-12 21:19:24 -0300

    Fixing delay and introducing toggling on push.

--- avr/push_button/user_main.c
+++ avr/push_button/user_main.c
@@ -1,4 +1,7 @@
+#define F_CPU 16000000ul
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
@@ -8,9 +11,10 @@ int main(void)
     PORTD |= (1<<PORTD2); /* turn the LED on at PIND2*/
 
     while (1) {
-        if (PIND & (1<<PIND3)) /* if PIND3 is high, turn LED off */
-            PORTD &= ~(1<<PORTD2);
-        else /* if PIND3 is low then, turn LED on */
-            PORTD |= (1<<PORTD2);
+        if (PIND & (1<<PIND3)) { /* if PIND3 is high, turn LED off */
+            /* PORTD &= ~(1<<PORTD2); */
+            PIND |= (1<<PIND2);
+            _delay_ms(300);
+        }
     }
 }
