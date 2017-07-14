8d49e7eabfe26885a2574ce91ddccdb248559430
Pedro Freitas <pedrosffreitas@gmail.com>
2017-07-14 07:57:07 -0300

    Removing trailing white space.

--- avr/push_button/user_main.c
+++ avr/push_button/user_main.c
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-    
+
     DDRD |= (1<<DDD2); /* set PIND2 as ouput */
     DDRD |= (0<<DDD3); /* set PIND3 as input */
     PORTD |= (1<<PORTD2); /* turn the LED on at PIND2*/
